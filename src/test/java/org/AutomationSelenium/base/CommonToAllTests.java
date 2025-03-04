ea955f01dafc7816e8c9229b1071c902b36659e6
navaneethhhk <hknavaneeth@gmail.com>
2025-03-04 08:21:12 +0530

    commit

--- src/test/java/org/AutomationSelenium/base/CommonToAllTests.java
+++ src/test/java/org/AutomationSelenium/base/CommonToAllTests.java
@@ -1,9 +1,11 @@
 package org.AutomationSelenium.base;
 
 
+import junit.framework.Assert;
 import org.AutomationSelenium.driver.DriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -11,9 +13,12 @@ import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
 
 public class CommonToAllTests {
-
+    WebDriver driver;
+    WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
     @BeforeMethod
     public void setUp(){
@@ -24,4 +29,5 @@ public class CommonToAllTests {
     public void closeBrowser(){
         DriverManager.down();
     }
+
 }
--- src/test/java/org/AutomationSelenium/tests/PSA_Tests/LoginTests_POM_PropertyReader_DriverManger.java
+++ src/test/java/org/AutomationSelenium/tests/PSA_Tests/LoginTests_POM_PropertyReader_DriverManger.java
@@ -8,6 +8,7 @@ import org.AutomationSelenium.driver.DriverManager;
 
 import org.AutomationSelenium.pages.ProjectObjectModel.LoginPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -15,7 +16,7 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 import static org.AutomationSelenium.driver.DriverManager.driver;
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 public class LoginTests_POM_PropertyReader_DriverManger extends CommonToAllTests {
 
@@ -30,13 +31,32 @@ public class LoginTests_POM_PropertyReader_DriverManger extends CommonToAllTests
     public void testAdminLogin() {
         LoginPage loginPage2 = new LoginPage(DriverManager.getDriver());
         loginPage2.LoginAsAdmin();
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//a[@href='/my-work' and contains(@class, 'MuiButtonBase-root') and contains(@class, 'MuiListItem-root')]")
+        )).click();
+       String welcomeMsg= webDriverWait.until(visibilityOfElementLocated(By.xpath("//h2[@data-qe-id=\'UserProfileAvatarName\']"))).getText();
+       Assert.assertEquals("Welcome, H K, Navaneeth",welcomeMsg);
+        String role= webDriverWait.until(visibilityOfElementLocated(By.xpath("//span[@data-qe-id=\"role\"]"))).getText();
+        Assert.assertEquals("Admin",role);
 
     }
     @Description("Login as user")
     @Test
     public void testUserLogin(){
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
         LoginPage loginPage3 = new LoginPage(DriverManager.getDriver());
         loginPage3.LoginAsUser();
+        webDriverWait.until(visibilityOfElementLocated(By.xpath("//div[@role='tablist']/a[2]"))).click();
+//        webDriverWait.until(visibilityOfElementLocated(By.id("add-new-timeline"))).click();
+//        String[] weekdays = {"Mon", "Tue", "Wed", "Thu", "Fri"};
+//        for(int i = 0; i < weekdays.length; i++) {
+//            WebElement hourInput = driver.findElement(By.xpath("//input[contains(@aria-label, 'Timeline 1 " + weekdays[i] + "')]"));
+//            hourInput.clear();
+//            hourInput.sendKeys("8");
+//        }
+
+
     }
     @Description("Login as Supervisor")
     @Test
@@ -46,6 +66,20 @@ public class LoginTests_POM_PropertyReader_DriverManger extends CommonToAllTests
         loginPage4.LoginAsSupervisor();
         String overview = webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Overview']"))).getText();
         Assert.assertEquals("OVERVIEW", overview);
+        String welcomeMsg= webDriverWait.until(visibilityOfElementLocated(By.xpath("//h2[@data-qe-id=\'UserProfileAvatarName\']"))).getText();
+        Assert.assertEquals("Welcome, 1, sup",welcomeMsg);
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//a[@href='/team' and contains(@class, 'MuiButtonBase-root') and contains(@class, 'MuiListItem-root') and contains(@class, 'MuiListItem-gutters') and contains(@class, 'MuiListItem-button')]")
+        )).click();
+        String Approvals = webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='tablist']/a[1]"))).getText();
+        Assert.assertEquals("APPROVALS", Approvals);
+        String Timesheets = webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='tablist']/a[2]"))).getText();
+        Assert.assertEquals("TIMESHEETS", Timesheets);
+        String Time_Off = webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='tablist']/a[3]"))).getText();
+        Assert.assertEquals("TIME OFF", Time_Off);
+        String Expenses = webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='tablist']/a[4]"))).getText();
+        Assert.assertEquals("EXPENSES", Expenses);
+
 
     }
     @Description("Login as CostManager")
